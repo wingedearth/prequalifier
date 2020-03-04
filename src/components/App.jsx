cd7391a10c9a95f69b7c666636d7f40028cb93b9
Andrew Anissi <aanissi@legacy.com>
2020-03-03 22:43:40 -0800

    chore: start form

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,6 +4,12 @@ import { AppContext } from './AppContext';
 import Navigation from '@/components/navigation/Navigation';
 import './App.scss';
 
+/**
+ * @function App
+ * @param {Function} Component
+ * @param {Object} data
+ * @returns {React.ReactElement}
+ */
 const App = ({ Component, data }) => {
 	return (
 		<AppContext.Provider value={data}>
